68afe1968339d907b18749db299fc18efbb5e865
Keith Brinker <keithbrinker@zipcoders-MBP-2.fios-router.home>
2018-04-15 16:09:58 -0400

    test written, method stubbed out, gametime

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,10 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public static String reverseString (String input){
+        String answer = "";
+
+        return answer;
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+
+    @Test
+    public void testReverseString(){
+        //given
+        String input = "I love Zip Code Wilmington";
+
+        //when
+        String actual = Problem5.reverseString(input);
+
+        //then
+        String given = "notgnimliW edoC piZ evol I";
+        Assert.assertEquals(actual, given);
+
+    }
 }
