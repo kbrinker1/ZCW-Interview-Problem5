249e89897b53f6c71bb6ffe115b5434ec5d4a129
Keith Brinker <keithbrinker@zipcoders-MBP-2.fios-router.home>
2018-04-15 16:16:10 -0400

    added comment

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -4,7 +4,7 @@ public class Problem5 {
 
     public static String reverseString (String input){
         String answer = "";
-
+        //for loop starts at the end of the input and adds each letter to a new string
         for (int i =input.length()-1; i >= 0; i--){
             answer += input.charAt(i);
         }
